8c1bca4be5142719af44d59d14049fe8253efc16
torbenbrodt <t.brodt@gmail.com>
2010-10-31 16:21:03 +0000

    base system for openid server

---
+++ optionals/de.easy-coding.wcf.openid.server/files/lib/page/OpenIDServerPage.class.php
@@ -0,0 +1,17 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/page/AbstractPage.class.php');
+
+/**
+ * allows authentication on other pages
+ * 
+ * @author	Torben Brodt
+ * @copyright	2010 easy-coding.de
+ * @license	GNU General Public License <http://opensource.org/licenses/gpl-3.0.html>
+ * @package	de.easy-coding.wcf.openid.server
+ */
+class OpenIDServerPage extends AbstractPage {
+
+	// @see	http://github.com/openid/php-openid/downloads
+	// examples/server
+}
